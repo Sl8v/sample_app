99b43fdf71a3f46012c060dc7174124b6ed5bf4a
Slav <slav999@ukr.net>
2015-11-06 15:47:32 +0200

    Ch6 makea a basic USER model with pass

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+	before_save { self.email = email.downcase }
+	
+	validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+	VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, format: { with: VALID_EMAIL_REGEXP },
+									uniqueness: { case_sensitive: false }
+									
+	has_secure_password
+	validates :password, length: { minimum: 6 }
+end
