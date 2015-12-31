40d3be2bbfe61c1ec1d91dcbe7c20f042e849b33
Slav <slav999@ukr.net>
2015-12-31 16:01:20 +0200

    8.2.4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
 	#before_save { self.email = email.downcase }
 	before_save { email.downcase! }
-	
+	before_create :create_remember_token
+
 	validates :name, presence: true, length: { minimum: 2, maximum: 50 }
 	VALID_EMAIL_REGEXP = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     validates :email, presence: true, format: { with: VALID_EMAIL_REGEXP },
@@ -9,4 +10,19 @@ class User < ActiveRecord::Base
 									
 	has_secure_password
 	validates :password, length: { minimum: 6 }
+
+  def User.new_remember_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.encrypt(token)
+    Digest::SHA1.hexdigest(token.to_s)
+  end
+
+  private
+
+    def create_remember_token
+      self.remember_token = User.encrypt(User.new_remember_token)
+    end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,10 +13,12 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   
   it {should be_valid}
-  
+
+
   describe "when name is not present" do
 	before { @user.name = ' ' }
 	it { should_not be_valid }
@@ -99,11 +101,17 @@ describe User do
    describe " email with MiXeD case" do
 		let(:mixed_mail) { "Foo@ExAmPle.cOm" }
 	
-	it "should be saved as lower-case" do
-		@user.email = mixed_mail
-		@user.save
-		expect(@user.reload.email).to eq mixed_mail.downcase
-	 end
-    end
+	   it "should be saved as lower-case" do
+		   @user.email = mixed_mail
+		   @user.save
+		    expect(@user.reload.email).to eq mixed_mail.downcase
+	   end
+   end
+
+  describe "remember token" do
+    before {@user.save}
+    expect(subject.remember_token).not_to be_blank
+  end
+
 
 end
